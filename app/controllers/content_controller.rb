2f75b0baebbad546a34beac7641f1ac0eafa3816
ShaneBurkhart <shaneburkhart@gmail.com>
2013-10-22 11:26:10 -0500

    Basic plan routes setup

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,15 +1,8 @@
 class ContentController < ApplicationController
   before_filter :authenticate_user!
-  
-  def silver
-    authorize! :view, :silver, :message => 'Access limited to Silver Plan subscribers.'
-  end
-  
-  def gold
-    authorize! :view, :gold, :message => 'Access limited to Gold Plan subscribers.'
+
+  def basic
+    authorize! :view, :basic, :message => 'Access limited to subscribers.'
   end
 
-  def platinum
-    authorize! :view, :platinum, :message => 'Access limited to Platinum Plan subscribers.'
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,17 @@ Beerocity::Application.routes.draw do
 
   mount StripeEvent::Engine => '/stripe'
 
-  get "content/gold"
-  get "content/silver"
-  get "content/platinum"
+  get "content" => "content#basic"
 
   authenticated :user do
     root :to => 'home#index'
   end
 
   root :to => "home#index"
+
   devise_for :users, :controllers => { :registrations => 'registrations' }
   devise_scope :user do
-    put 'update_plan', :to => 'registrations#update_plan'
+    # put 'update_plan', :to => 'registrations#update_plan'
     put 'update_card', :to => 'registrations#update_card'
   end
 
