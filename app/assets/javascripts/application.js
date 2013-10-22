b0c82986222b1cac9e04acf5542c3f33c13d4995
ShaneBurkhart <shaneburkhart@gmail.com>
2013-10-21 22:22:14 -0500

    Smooth scrolling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,21 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+
+$(document).ready(function(){
+  $(".scroll").click(function(event){
+    event.preventDefault();
+    //calculate destination place
+
+    var dest = 0;
+
+    if($(this.hash).offset().top > $(document).height() - $(window).height()){
+      dest = $(document).height() - $(window).height();
+    }else{
+      dest = $(this.hash).offset().top;
+    }
+
+    //go to destination
+    $('html,body').animate({scrollTop : dest}, 500,'swing');
+  });
+});
