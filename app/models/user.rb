583f51883d7f4a68ffa05f8163538d99872caddf
Shane Burkhart <shaneburkhart@gmail.com>
2013-10-28 13:20:28 -0500

    Name error correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -98,6 +98,10 @@ class User < ActiveRecord::Base
     destroy
   end
 
+  def full_name
+    first_name + last_name
+  end
+
   def self.avatar_url email
     hash = Digest::MD5.hexdigest(email.strip)
     "http://www.gravatar.com/avatar/#{hash}?s=200&d=mm"
