c49f8cf6ff4aac34a33326d92fe42d96658719f2
Shane Burkhart <shaneburkhart@gmail.com>
2013-10-28 13:11:09 -0500

    Seeds user that is an admin without application.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,11 @@
 # Environment variables (ENV['...']) are set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
 puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
+# YAML.load(ENV['ROLES']).each do |role|
+["basic", "admin"].each do |role|
   Role.find_or_create_by_name({ :name => role }, :without_protection => true)
   puts 'role: ' << role
 end
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+user = User.find_or_create_by_email :first_name => "Shane", :last_name => "Burkhart", :email => "shaneburkhart@gmail.com", :password => "changeme", :password_confirmation => "changeme", address: "111 First St.", city: "Rolla", state: "Missouri", zipcode: "65401", stripe_token: "asdfasdfasdfasdf"
 user.add_role :admin
