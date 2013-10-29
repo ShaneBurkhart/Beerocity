e3502e6d1ecb5cd7078c9b41cd0f7f709a529649
Shane Burkhart <shaneburkhart@gmail.com>
2013-10-29 11:02:36 -0500

    Validate and upload image

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,10 @@
 class Recipe < ActiveRecord::Base
-  attr_accessible :description, :name, :month, :year
+  attr_accessible :description, :name, :month, :year, :image
   has_attached_file :image
 
   validates :description, :name, :month, :year, presence: true
+  validates :image, presence: true unless @id
+  validate :not_duplicate_month
 
   has_many :comments
 
@@ -13,5 +15,12 @@ class Recipe < ActiveRecord::Base
   def self.current_recipe
     Recipe.find_by_month_and_year(Date.today.month, Date.today.year)
   end
+
+  private
+
+    def not_duplicate_month
+      r = Recipe.find_by_month_and_year(month, year)
+      errors.add(:month, "already exists") if r && r.id != id
+    end
 
 end
