c02fad126a16276b34899a8421b6185249a38e65
Shane Burkhart <shaneburkhart@gmail.com>
2013-10-29 13:18:51 -0500

    Added deadline to recipe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131029160454) do
+ActiveRecord::Schema.define(:version => 20131029181801) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20131029160454) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.boolean  "is_charged"
+    t.integer  "deadline"
   end
 
   create_table "roles", :force => true do |t|
