06b889ae0be9806e627504d52060540a244f0009
Shane Burkhart <shaneburkhart@gmail.com>
2013-10-29 12:50:01 -0500

    User is only ignored if exactly my email

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ActiveRecord::Base
   end
 
   def update_stripe
-    return if email.include?("shaneburkhart@gmail.com")
+    return if email == "shaneburkhart@gmail.com"
     return if email.include?('@example.com') and not Rails.env.production?
     if customer_id.nil?
       if !stripe_token.present?
