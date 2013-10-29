8a02ab69c3a2edcd3c09ae014b77e824f64926c1
Shane Burkhart <shaneburkhart@gmail.com>
2013-10-29 14:39:51 -0500

    Seed file for recipe

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,12 @@ end
 puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :first_name => "Shane", :last_name => "Burkhart", :email => ENV["ADMIN_EMAIL"], :password => ENV["ADMIN_PASSWORD"], :password_confirmation => ENV["ADMIN_PASSWORD"], address: "111 First St.", city: "Rolla", state: "Missouri", zipcode: "65401", stripe_token: "asdfasdfasdfasdf"
 user.add_role :admin
+
+Recipe.find_or_create_by_month_and_year(
+  month: Date.today.month,
+  year: Date.today.year,
+  name: "Gingerbread Ale",
+  description: "Gingerbread Ale That is great!!!",
+  deadline: "20",
+  image: "Some image"
+)
