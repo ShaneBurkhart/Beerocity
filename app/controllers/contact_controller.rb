e9190ca8891a203e5e67592d62122772b1be95a5
Shane Burkhart <shaneburkhart@gmail.com>
2013-11-01 00:26:25 -0500

    Removed dynamic recipe on front page

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -3,6 +3,7 @@ class ContactController < ApplicationController
   end
 
   def message
+
     redirect_to root_path, notice: "Your message has been sent and we will get back with you soon!"
   end
 end
