7cd2c5f9f9382a974f59c3707ea07a468545e7c3
Shane Burkhart <shaneburkhart@gmail.com>
2013-11-08 13:07:58 -0600

    Added some containers

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Beerocity::Application.routes.draw do
   get "team" => "about#index"
   get "faq" => "faq#index"
   get "contact" => "contact#index"
-  post "contact" => "contact#message"
+#  post "contact" => "contact#message"
 #  get "tos" => "faq#index"
 
 # The route to execute charges
